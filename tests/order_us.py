52416ae4fa178913d9bd0db81731e804ee994865
Soju06 <34199905+Soju06@users.noreply.github.com>
2023-06-02 14:07:39 +0900

    해외 테스트 추가

--- tests/order_us.py
+++ tests/order_us.py
@@ -48,10 +48,17 @@ order2 = account.overseas_revise(
     unpr=110,
 )
 
+orders = account.overseas_revisable_order_all('나스닥')
+
+for my_order in orders.orders:
+    print(f'주문일시: {my_order.order_date:%Y-%m-%d %H:%M:%S} 종목코드: {my_order.pdno} 종목명: {my_order.prdt_name} {my_order.sll_buy_dvsn_cd_name} 미체결수량: {my_order.nccs_qty} 주문단가: {my_order.ft_ord_unpr3}')
+
 print(order2.message)
 
 # 미체결 조회
-for stock in account.overseas_revisable_order_all('나스닥').orders:
+nottraded = account.overseas_revisable_order_all('나스닥')
+
+for stock in nottraded.orders:
     print(f'주문일시: {stock.order_date:%Y-%m-%d %H:%M:%S} 종목코드: {stock.pdno} 종목명: {stock.prdt_name} {stock.sll_buy_dvsn_cd_name} 미체결수량: {stock.nccs_qty} 주문단가: {stock.ft_ord_unpr3}')
 
 
