d98ea5310eb463c07a9a0df57310713c9bca8fd6
Soju06 <qlskssk@gmail.com>
2024-05-10 11:18:53 +0900

    해외주식 실시간 시세 조회 구현

--- pykis/api/websocket/__init__.py
+++ pykis/api/websocket/__init__.py
@@ -1,6 +1,7 @@
-from pykis.api.websocket.price import KisDomesticRealtimePrice
+from pykis.api.websocket.price import KisDomesticRealtimePrice, KisOverseasRealtimePrice
 from pykis.responses.websocket import KisWebsocketResponse
 
 WEBSOCKET_RESPONSES_MAP: dict[str, type[KisWebsocketResponse]] = {
     "H0STCNT0": KisDomesticRealtimePrice,
+    "HDFSCNT0": KisOverseasRealtimePrice,
 }
